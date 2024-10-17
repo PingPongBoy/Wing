4151ff875f370f38dd5b7acd17b58d40a7fac981
linguicheng <773199358@qq.com>
2024-10-17 13:49:04 +0800

    服务中心

--- src/Wing.ServiceCenter/WingBuilderExtensions.cs
+++ src/Wing.ServiceCenter/WingBuilderExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using FreeSql;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Wing.Configuration.ServiceBuilder;
+using Wing.Filters;
 using Wing.ServiceCenter.Service;
 
 namespace Wing
@@ -9,6 +11,18 @@ namespace Wing
     {
         public static IWingServiceBuilder AddServiceCenter(this IWingServiceBuilder wingBuilder, DataType dataType, string connectionString = "")
         {
+            wingBuilder.Services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(ApiExceptionFilter));
+                options.Filters.Add(typeof(ApiResultFilter));
+            });
+
+            // 禁用默认行为
+            wingBuilder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
             wingBuilder.AddPersistence(dataType, connectionString);
             wingBuilder.Services.AddSingleton<RegisterService>();
             wingBuilder.Services.AddSingleton<ConfigService>();
